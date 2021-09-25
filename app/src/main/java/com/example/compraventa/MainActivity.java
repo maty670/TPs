77513a7d699965f5d3169b0416d9b82f7bf1af17
maty670 <matiasesposito.10@hotmail.com>
2021-09-25 12:14:44 -0300

    Agregado Checkbox de retiro con las validaciones. Lab1 completado

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -12,16 +12,21 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     public EditText txtTitulo;
     public EditText txtDescripcion;
     public EditText txtCorreo;
     public EditText txtPrecio;
+    public TextView Retiro;
+    public EditText txtRetiro;
     public Spinner SpinnerCategorias;
     public TextView txtDescuento;
     public Switch SwitchDescuento;
     public SeekBar SeekBarDescuento;
+    public CheckBox CheckBoxRetiro;
     public CheckBox CheckBoxTerminos;
     public Button BtnAceptar;
 
@@ -35,38 +40,67 @@ public class MainActivity extends AppCompatActivity {
         txtCorreo = findViewById(R.id.editCorreo);
         txtPrecio = findViewById(R.id.editPrecio);
         SpinnerCategorias = findViewById(R.id.spinnerCategoria);
-        BtnAceptar = findViewById(R.id.buttonAceptar);
         SeekBarDescuento = findViewById(R.id.seekBarDescuento);
         txtDescuento = findViewById(R.id.textViewDescuento);
         SwitchDescuento = findViewById(R.id.switchDescuento);
+        CheckBoxRetiro = findViewById(R.id.checkBoxRetiro);
+        Retiro =findViewById(R.id.txtRetiro);
+        txtRetiro = findViewById(R.id.editRetiro);
         CheckBoxTerminos = findViewById(R.id.checkBoxTerminos);
         BarraDescuentos(SeekBarDescuento);
+        BtnAceptar = findViewById(R.id.buttonAceptar);
 
 
     }
 
     public void ValidarCampos(View view) {
+
         String Titulo = txtTitulo.getText().toString();
-        String Precio = txtPrecio.getText().toString();
+        String Descripcion= txtDescripcion.getText().toString();
         String Correo = txtCorreo.getText().toString();
+        String Precio= txtPrecio.getText().toString();
+        String Retiro= txtRetiro.getText().toString();
+
+
+        Boolean TituloVacio = txtTitulo.getText().toString().equals("");
+        Boolean DescripcionVacio = txtDescripcion.getText().toString().equals("");
+        Boolean PrecioVacio = txtPrecio.getText().toString().equals("");
+        Boolean CorreoVacio = txtCorreo.getText().toString().equals("");
+        Boolean RetiroVacio = txtRetiro.getText().toString().equals("");
+
+
+        Boolean Titulovalido = Pattern.matches("[a-zA-Z0-9,. ]+",Titulo);
+        Boolean Descripcionvalido = Pattern.matches("[a-zA-Z0-9,. ]+",Descripcion);
+        Boolean RetiroValido = Pattern.matches("[a-zA-Z0-9,. ]+",Retiro);
+
+
         Boolean DescuentoCheck = SwitchDescuento.isChecked();
         Boolean DescuentoNoCheck = !SwitchDescuento.isChecked();
         int Descuento = SeekBarDescuento.getProgress();
+
+        Boolean RetiroCheck = CheckBoxRetiro.isChecked();
+        Boolean RetiroNoCheck = CheckBoxRetiro.isChecked();
+
         Boolean TerminosCheck = CheckBoxTerminos.isChecked();
         Boolean TerminosNoCheck = !CheckBoxTerminos.isChecked();
 
 
-        if (Titulo.equals("") | Precio.equals(""))
-            Toast.makeText(this,"Campos Titulo y Precio no pueden estar vacios", Toast.LENGTH_SHORT).show();
+
+
+        if (TituloVacio | PrecioVacio | (RetiroCheck & RetiroVacio ))
+            Toast.makeText(this,"Titulo, Precio y Retiro no pueden estar vacios", Toast.LENGTH_SHORT).show();
         else if(Integer.parseInt(Precio)<=0)
             Toast.makeText(this, "El precio debe ser mayor a 0", Toast.LENGTH_SHORT).show();
-        else if(Correo.length()>0 & (!(Correo.contains("@")) | !(Correo.lastIndexOf("@")<Correo.length()-3))) {
+        else if(!CorreoVacio & (!(Correo.contains("@")) | !(Correo.lastIndexOf("@")<Correo.length()-3)))
             Toast.makeText(this, "Correo debe contener al menos 1 @ seguido de 3 letras", Toast.LENGTH_SHORT).show();
-        }
-        else if(DescuentoCheck & Descuento==0){
+        else if (!Titulovalido | (!DescripcionVacio & !Descripcionvalido) | (RetiroCheck & !RetiroValido & !RetiroVacio))
+            Toast.makeText(this, "TEXTO INVALIDO: solo se permiten letras(mayusculas o minusculas), numeros, comas, puntos o saltos de linea", Toast.LENGTH_LONG).show();
+        else if(DescuentoCheck & Descuento<=0)
             Toast.makeText(this, "Descuento debe ser mayor a 0%", Toast.LENGTH_SHORT).show();
-        }else if (TerminosNoCheck)
+        else if (TerminosNoCheck)
             Toast.makeText(this, "Debe aceptar los terminos y condiciones", Toast.LENGTH_SHORT).show();
+
+
 
     }
 
@@ -80,6 +114,20 @@ public class MainActivity extends AppCompatActivity {
             txtDescuento.setVisibility(View.GONE);
         }
     }
+
+    public void ActivarRetiro(View view) {
+        if (CheckBoxRetiro.isChecked()) {
+            Retiro.setVisibility(View.VISIBLE);
+            txtRetiro.setVisibility(View.VISIBLE);
+        }
+        else{
+            Retiro.setVisibility(View.GONE);
+            txtRetiro.setVisibility(View.GONE);
+        }
+    }
+
+
+
 
     public void BarraDescuentos(View view) {
             {
