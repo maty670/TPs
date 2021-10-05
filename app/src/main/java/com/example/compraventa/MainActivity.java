8e0b546820173b7ab414c58707c6e80bc4134bca
MatiasEsposito <matiasesposito.10@hotmail.com>
2021-10-04 22:51:39 -0300

    Lab 02

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.compraventa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,39 +17,57 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public TextView txtDescuento;
+    public TextView Retiro;
+    public TextView txtCategoria;
+
+    public ImageView imagen;
+
     public EditText txtTitulo;
     public EditText txtDescripcion;
     public EditText txtCorreo;
     public EditText txtPrecio;
-    public TextView Retiro;
     public EditText txtRetiro;
-    public Spinner SpinnerCategorias;
-    public TextView txtDescuento;
+
+
     public Switch SwitchDescuento;
     public SeekBar SeekBarDescuento;
     public CheckBox CheckBoxRetiro;
     public CheckBox CheckBoxTerminos;
     public Button BtnAceptar;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         txtTitulo = findViewById(R.id.editTitulo);
         txtDescripcion = findViewById(R.id.editDescripcion);
         txtCorreo = findViewById(R.id.editCorreo);
         txtPrecio = findViewById(R.id.editPrecio);
-        SpinnerCategorias = findViewById(R.id.spinnerCategoria);
-        SeekBarDescuento = findViewById(R.id.seekBarDescuento);
         txtDescuento = findViewById(R.id.textViewDescuento);
+        txtRetiro = findViewById(R.id.editRetiro);
+
+        SeekBarDescuento = findViewById(R.id.seekBarDescuento);
         SwitchDescuento = findViewById(R.id.switchDescuento);
         CheckBoxRetiro = findViewById(R.id.checkBoxRetiro);
         Retiro =findViewById(R.id.txtRetiro);
-        txtRetiro = findViewById(R.id.editRetiro);
         CheckBoxTerminos = findViewById(R.id.checkBoxTerminos);
-        BarraDescuentos(SeekBarDescuento);
         BtnAceptar = findViewById(R.id.buttonAceptar);
+
+        imagen=findViewById(R.id.imagen_Categoria);
+        txtCategoria= findViewById(R.id.txtCategoria);
+
+        BarraDescuentos(SeekBarDescuento);
+
+
+
+
 
 
     }
@@ -59,12 +78,14 @@ public class MainActivity extends AppCompatActivity {
         String Descripcion= txtDescripcion.getText().toString();
         String Correo = txtCorreo.getText().toString();
         String Precio= txtPrecio.getText().toString();
+        String Categoria= txtCategoria.getText().toString();
         String Retiro= txtRetiro.getText().toString();
 
 
         Boolean TituloVacio = txtTitulo.getText().toString().equals("");
         Boolean DescripcionVacio = txtDescripcion.getText().toString().equals("");
         Boolean PrecioVacio = txtPrecio.getText().toString().equals("");
+        Boolean CategoriaVacio = txtCategoria.getText().toString().equals("");
         Boolean CorreoVacio = txtCorreo.getText().toString().equals("");
         Boolean RetiroVacio = txtRetiro.getText().toString().equals("");
 
@@ -86,13 +107,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
         if (TituloVacio | PrecioVacio | (RetiroCheck & RetiroVacio ))
             Toast.makeText(this,"Titulo, Precio y Direccion de Retiro no pueden estar vacios", Toast.LENGTH_SHORT).show();
         else if(Long.parseLong(Precio)<=0)
             Toast.makeText(this, "El precio debe ser mayor a 0", Toast.LENGTH_SHORT).show();
         else if(!CorreoVacio & (!(Correo.contains("@")) | !(Correo.lastIndexOf("@")<Correo.length()-3)))
             Toast.makeText(this, "Correo debe contener al menos 1 @ seguido de 3 letras", Toast.LENGTH_SHORT).show();
+        else if(CategoriaVacio)
+            Toast.makeText(this, "Debe seleccionar una Categoria", Toast.LENGTH_SHORT).show();
         else if (!Titulovalido | (!DescripcionVacio & !Descripcionvalido) | (RetiroCheck & !RetiroValido & !RetiroVacio))
             Toast.makeText(this, "TEXTO INVALIDO: solo se permiten letras(mayusculas o minusculas), numeros, comas, puntos o saltos de linea", Toast.LENGTH_LONG).show();
         else if(DescuentoCheck & Descuento<=0)
@@ -132,7 +154,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void BarraDescuentos(View view) {
             {
-
                 // Valor Inicial
                 SeekBarDescuento.setProgress(0);
                 // Valot Final
@@ -154,11 +175,37 @@ public class MainActivity extends AppCompatActivity {
                     public void onStopTrackingTouch(SeekBar seekBar) {
 
                     }
-
                 });
             }
     }
 
+
+
+
+    public void ir_Categorias(View view){
+
+        Intent intent = new Intent(MainActivity.this,Categorias.class);
+        startActivityForResult(intent, 1);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("nombre");
+                int Foto = data.getIntExtra("Foto",0);
+                txtCategoria.setText("" + result);
+                imagen.setImageResource(Foto);
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //No se selecciono nada
+            }
+        }
+    }
 
 
 
